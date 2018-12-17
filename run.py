7a442c87f4e788f5fe235137dea42f48216a09f7
fly <571435345@qq.com>
2018-12-17 23:38:54 +0800

    add max and min prob

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
 import os
 import numpy as np
 
-for i in np.arange(100, 0, -5):
+value = [[60, 80], [70, 90], [60, 90]]
+for i in np.arange(len(value)):
     print('i = %.3f' % i)
     log_name = 'log/' + 'log_' + str(i)
     print('log name = %s' % log_name)
-    cmd = 'python train_baseline.py --use_dense  --modelname ' + 'prob_' + str(i) + ' --prob ' + str(i) + ' >> ' + log_name
+    cmd = 'python train_baseline.py --use_dense  --modelname ' + 'prob_' + str(i) + ' --prob ' + str(i) + ' --min ' + str(value[i][0]) + ' --max ' + str(value[i][1]) + ' >> ' + log_name
     print('cmd = %s' % cmd)
-    os.system('python train_baseline.py --use_dense  --modelname ' + 'prob_' + str(i) + ' --prob ' + str(i) + ' >> ' + log_name)
+    os.system(cmd)
     os.system('python test.py  --use_dense' + ' >> ' + log_name)
     os.system('python evaluate.py' + ' >> ' + log_name)
     os.system('python evaluate_rerank.py' + ' >> ' + log_name)
