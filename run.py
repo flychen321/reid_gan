ea63ff613dc0491e406bdbbc01e1ff8b685499dc
dl <571435345@qq.com>
2018-12-19 16:37:23 +0800

    modify for detailed test

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 
-value = [[60, 80], [70, 90], [60, 90]]
+value = [[70, 90], [60, 90], [60, 80], [80, 80], [90, 90], [70, 70], [60, 60], [50, 50]]
 for i in np.arange(len(value)):
     print('i = %.3f' % i)
     log_name = 'log/' + 'log_' + str(i)
@@ -9,6 +9,7 @@ for i in np.arange(len(value)):
     cmd = 'python train_baseline.py --use_dense  --modelname ' + 'prob_' + str(i) + ' --prob ' + str(i) + ' --min ' + str(value[i][0]) + ' --max ' + str(value[i][1]) + ' >> ' + log_name
     print('cmd = %s' % cmd)
     os.system(cmd)
-    os.system('python test.py  --use_dense' + ' >> ' + log_name)
-    os.system('python evaluate.py' + ' >> ' + log_name)
-    os.system('python evaluate_rerank.py' + ' >> ' + log_name)
+    for k in np.arange(0, 40, 1):
+        os.system('python test.py  --use_dense  --which_epoch  ' + str(k) + ' >> ' + log_name)
+        os.system('python evaluate.py' + ' >> ' + log_name)
+        os.system('python evaluate_rerank.py' + ' >> ' + log_name)
